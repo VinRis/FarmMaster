694b720b44a3f13532354ef2214113fbd500227f
VinRis <71922931+VinRis@users.noreply.github.com>
2025-12-23 12:07:23 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,19 @@
+import { auth } from './firebase-config.js';
+import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+export const loginUser = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+        const result = await signInWithPopup(auth, provider);
+        alert(`Welcome back, ${result.user.displayName}!`);
+        return result.user;
+    } catch (error) {
+        console.error("Login failed", error);
+    }
+};
+
+export const logoutUser = () => signOut(auth);
+
+export const checkAuthStatus = (callback) => {
+    onAuthStateChanged(auth, callback);
+};
