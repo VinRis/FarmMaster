655da1afe28c00f3311060257f234a1ad3b54be9
VinRis <71922931+VinRis@users.noreply.github.com>
2025-12-23 12:13:25 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+const firebaseConfig = {
+  // PASTE YOUR CONFIG OBJECT HERE
+  apiKey: "AIzaSyAva7tu7mWrdgJswDIR0W9OWv8ctZ5phPk",
+  authDomain: "farmtrack-b470e.firebaseapp.com",
+  projectId: "farmtrack-b470e",
+  storageBucket: "farmtrack-b470e.firebasestorage.app",
+  messagingSenderId: "572276398926",
+  appId: "1:572276398926:web:4b39dc570ce2077fae5c1f",
+  measurementId: "G-8TLCQ93SR2"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
